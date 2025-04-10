1bc1e941b0bac7fbc2d5e1ea0db8a2bf64c99c4e
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-04-10 09:40:06 +0530

    file committed

---
+++ 40-apps/variables.tf
@@ -0,0 +1,33 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "development"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "mysql_tags" {
+    default = {
+        component = "mysql"
+    }
+}
+
+variable "backend_tags" {
+    default = {
+        component = "backend"
+    }
+}
+
+variable "frontend_tags" {
+    default = {
+        component = "frontend"
+    }
+}
