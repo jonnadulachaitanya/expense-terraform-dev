817a8c803f068f99acbaf72887817be60e33c363
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-04-16 11:19:35 +0530

    file committed

--- 40-apps/expense.sh
+++ 40-apps/expense.sh
@@ -3,6 +3,13 @@ dnf install ansible -y
 cd /tmp
 git clone https://github.com/jonnadulachaitanya/expense-ansible.git
 cd expense-ansible
-ansible-playbook -i inventory.ini mysql.yaml
+ansible-playbook -i inventory.ini 1-mysql.yaml
 ansible-playbook -i inventory.ini backend.yaml
 ansible-playbook -i inventory.ini frontend.yaml
+
+
+#To connect MYSQL db from ansible or any other server:
+# mysql -h 10.0.21.119 -u root -pExpenseApp@1
+# show databases;
+# use transaction;
+# select * from transactions;
